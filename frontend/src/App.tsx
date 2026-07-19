3fffe6a4885798c4a6212053bceb651955c39f96
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-07-19 01:06:16 -0300

    (chore) Aplication's tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App"><H1>Bem bonzim</H1></div>
+    <div className="App"><h1>Bem bonzim</h1></div>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins:[
+
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+
+    VitePWA({
+
+        registerType:"autoUpdate",
+
+        manifest:{
+
+        }
+
+    })
+
+    ],
 })
