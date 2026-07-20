185bcba1a16c9f4b7404b905781df9aaa6ea02a2
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-07-20 16:14:38 -0300

    (feature) Adding hashPassword to users

--- backend/src/schema/UserSchema.ts
+++ backend/src/schema/UserSchema.ts
@@ -5,4 +5,6 @@ export const createUserSchema = z.object({
     name: z.string().min(3, "O nome deve possuir pelo menos 3 caracteres."),
 
     email: z.string().email("Email está inválido."),
+
+    password: z.string().min(8, "A senha deve possuir pelo menos 8 caracteres"),
 })
--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,10 +1,20 @@
 import {prisma} from "../config/Prisma.js";
+import bcrypt from "bcrypt";
 
 export const createUserService = async(data:any) => {
+    const hashPassword = await bcrypt.hash(data.password, 10);
+
     const user = await prisma.users.create({
         data: {
             name: data.name,
             email: data.email,
+            password: hashPassword,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
         }
     })
     return user;
