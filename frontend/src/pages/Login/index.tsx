bf4c73e56b971d1c79371a04924f1fa03788f33a
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-07-23 15:21:21 -0300

    (feature) Login and Register

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,3 +1,92 @@
-export default function Login() {
-    return <h1>Login</h1>;
+import {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/UseAuth";
+
+export default function Login () {
+    const navigate = useNavigate();
+
+    const {login, isAuthenticated} = useAuth();
+
+    const [email, setEmail] = useState("");
+
+    const [password, setPassword] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if(isAuthenticated) {
+            navigate("/");
+        }
+    }, [isAuthenticated, navigate])
+
+    const handleLogin = async (
+        event: React.FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault()
+
+        try{
+            setLoading(true);
+
+            await login(email, password);
+
+            navigate("/");
+        } catch(error) {
+            console.error(error)
+
+            alert("Email ou senha inválidos.")
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return(
+        <main>
+            <h1>Login</h1>
+
+            <form onSubmit={handleLogin}>
+                
+                <input 
+                    type="email"
+                    placeholder="Digite seu email"
+                    value={email}
+                    onChange={(event) => {setEmail(event.target.value)}}
+                    required
+                />
+
+                <br />
+
+                <input 
+                    type="password"
+                    placeholder="Digite seu senha"
+                    value={password}
+                    onChange={(event) => {setPassword(event.target.value)}}
+                    required
+                />
+
+                <br />
+
+                <button
+                    type="submit"
+                    disabled={loading}
+                >
+                    {loading
+                        ? "Entrando..."
+                        : "Entrar"
+                    }
+                </button>
+                
+            </form>
+
+            <br />
+
+            <button
+                onClick={() => {
+                    navigate("/register")
+                }}
+            >
+                Criar Conta
+            </button>
+
+        </main>
+    )
 }
--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,3 +1,99 @@
-export default function Register() {
-    return <h1>Register</h1>;
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
+import {register} from "../../services/UserService"
+
+export default function Register () {
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("")
+
+    const [email, setEmail] = useState("")
+
+    const [password, setPassword] = useState("")
+
+    const [loading, setLoading] = useState(false)
+
+    const handleRegister = async (
+        event: React.FormEvent<HTMLFormElement>
+    ) => {
+        event.preventDefault()
+
+        try {
+            setLoading(true)
+
+            await register (
+                name,
+                email,
+                password
+            );
+
+            alert("Usuário cadastrado com sucesso,.")
+
+            navigate("/login");
+        } catch(error) {
+            console.error(error)
+
+            alert("Erro ao cadastrar usuário.")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <main>
+
+            <h1>Cadastro</h1>
+
+            <form onSubmit={handleRegister}>
+                <input
+                    type="text"
+                    placeholder="Digite seu nome"
+                    value={name}
+                    onChange={(event) => {setName(event.target.value)}}
+                    required
+                />
+
+                <br />
+
+                <input
+                    type="email"
+                    placeholder="Digite seu email"
+                    value={email}
+                    onChange={(event) => {setEmail(event.target.value)}}
+                    required
+                />
+
+                <br />
+
+                <input
+                    type="password"
+                    placeholder="Digite sua senha"
+                    value={password}
+                    onChange={(event) => {setPassword(event.target.value)}}
+                    required
+                />
+            
+                <br />
+
+                <button
+                    type="submit"
+                    disabled={loading}
+                >
+                    {loading
+                        ? "Cadastrando..."
+                        : "Cadastrar"    
+                    }
+                </button>
+            </form>
+
+            <br />
+
+            <button
+                onClick={() => {navigate("/login")}}
+            >
+                Voltar para Login
+            </button>
+
+        </main>
+    )
 }
