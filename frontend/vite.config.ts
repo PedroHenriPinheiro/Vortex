301732cf9ca937d18bb2919c90cc38d27182b322
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-07-24 14:26:22 -0300

    (fix) Instaling tailwind on the project and config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins:[
-
-    react(),
-
-    VitePWA({
-
-        registerType:"autoUpdate",
-
-        manifest:{
-
-        }
-
-    })
-
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+            }
+        })
     ],
-})
+});
