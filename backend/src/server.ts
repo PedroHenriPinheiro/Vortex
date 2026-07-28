dda7e0e01d7e9df94a29d8a6748020571af8dc6d
Pedro Pinheiro <pedrocaseis@hotmail.com>
2026-07-28 17:20:40 -0300

    (testing) Testingo env keys

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,3 +5,8 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
+console.log("PORT =", process.env.PORT);
+console.log("DATABASE_URL =", !!process.env.DATABASE_URL);
+console.log("JWT_SECRET =", !!process.env.JWT_SECRET);
